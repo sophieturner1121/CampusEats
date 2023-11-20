cbbd16498291101039aac569b664a24ef14cfd28
Errol Jay Supangan <106656646+Shadowrrol@users.noreply.github.com>
2023-11-20 02:16:59 +0000

    made changes

--- src/main/java/CampusEats/Pickup/Entity/FoodEntity.java
+++ src/main/java/CampusEats/Pickup/Entity/FoodEntity.java
@@ -11,5 +11,25 @@ import javax.persistence.Table;
 @Entity
 @Table(name="tblfood")
 public class FoodEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_id")
+    private int id;
+    
+    @Column(name = "food_name", nullable = false)
+    private String foodName;
 
+    @Column(nullable = false)
+    private double price;
+
+    @Column(length = 500)
+    private String description;
+
+    @Lob
+    @Column(name = "image", columnDefinition = "BLOB")
+    private byte[] image;
+
+    public FoodEntity() {
+        super();
+    }
 }
