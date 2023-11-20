6b5a8c0a67176b1c6ebceee393f53e7d41700763
Errol Jay Supangan <106656646+Shadowrrol@users.noreply.github.com>
2023-11-20 02:15:38 +0000

    making attributes

---
+++ src/main/java/CampusEats/Pickup/Entity/FoodEntity.java
@@ -0,0 +1,15 @@
+package CampusEats.Pickup.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tblfood")
+public class FoodEntity {
+
+}
